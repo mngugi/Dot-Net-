cbe76e985d7c51edc5ba67d648d57d501be1adea
M.ngugi <mngugi7@gmail.com>
2023-02-04 13:53:26 +0300

    Add files via upload

---
+++ challenge1.cs
@@ -0,0 +1,16 @@
+//https://learn.microsoft.com/en-us/training/modules/csharp-write-first/4-challenge
+//Write code in the .NET Editor to display two messages
+//This is the first line.
+//This is the second line.
+//
+//
+using System;
+class challenge1
+{
+  static void Main(string[] args)
+  {
+      Console.WriteLine("This is the first line.");
+      Console.WriteLine("This is the second line.");
+  }
+}
+
