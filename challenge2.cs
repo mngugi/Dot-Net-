54e0b11b3a7e8a92d84b19d88e7a81d22c064a0f
M.ngugi <mngugi7@gmail.com>
2023-02-04 15:46:49 +0300

    Add files via upload

---
+++ challenge2.cs
@@ -0,0 +1,17 @@
+/*
+C# program where user enters a value for the variable, in this case variable i
+Console.ReadLine() function takes an input typed by the user.
+Console.WriteLine() function prints the value of i and prints it on the console or terminal
+*/
+using System;
+class read_line
+{
+  static void Main(string[] args)
+  {
+	string i;
+	i = Console.ReadLine();
+	Console.WriteLine(i);
+  }
+	
+}
+
