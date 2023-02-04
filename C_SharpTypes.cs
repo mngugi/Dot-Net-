5e6e2723ab2c202283e8b8f8cef29cf4f24aa4b2
M.ngugi <mngugi7@gmail.com>
2023-02-04 20:29:45 +0300

    Add files via upload

---
+++ C_SharpTypes.cs
@@ -0,0 +1,46 @@
+/*
+ a c# program that displays keywords 
+
+*/
+
+using System ; 
+class keywords {
+    static void Main(String[] args) {
+    /*int    a = 4;
+    double b = 3.14567 ;
+    float  c = 3.2e ;
+    char   d = 'K' ;
+    bool   e = true ;
+    uint   f =  54;
+    short  g = 6 ;
+    ushort h =  16;
+    long   i =  1924;
+    ulong  j = 1986 ;
+    
+     */
+    int a = 4;
+  float b = 2.2f;
+  double c = 2.54;
+  char d = 'a';
+  bool e = true;
+  uint f = 43;
+  short g = 2;
+  ushort h = 19;
+  long i = 123;
+  ulong j = 34;
+
+  Console.WriteLine(a);
+  Console.WriteLine(b);
+  Console.WriteLine(c);
+  Console.WriteLine(d);
+  Console.WriteLine(e);
+  Console.WriteLine(f);
+  Console.WriteLine(g);
+  Console.WriteLine(h);
+  Console.WriteLine(i);
+  Console.WriteLine(j);
+    
+    
+    } 
+
+}
