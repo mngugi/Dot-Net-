1e254c20c39d0c41ca2f13e56372e9321f928e78
M.Ngugi <mngugi7@gmail.com>
2024-09-17 23:11:22 +0300

    ConsoleApp1/bin/Debug/net8.0/ConsoleApp1.dll

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,3 +4,20 @@ Console.WriteLine("This are the Champioooons!");
 Console.WriteLine(2024);
 Console.WriteLine(2.625);
 Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+Console.WriteLine("------Variables------")
+
+string firstName;
+firstName = "Garnacho";
+
+Console.WriteLine(firstName);
+
+char userOption;
+
+int gameScore;
+
+decimal particlesPerMillion;
+
+bool processedCustomer;
