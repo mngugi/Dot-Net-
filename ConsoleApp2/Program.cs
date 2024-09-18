3da2a8eb2a3017e8725604d74136e3d780f0f39c
M.Ngugi <mngugi7@gmail.com>
2024-09-18 22:20:32 +0300

    ConsoleA…/ConsoleApp2.dll

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,3 +11,8 @@ Console.WriteLine("\nOutput Directory:\t");
 
 Console.WriteLine(@"    c:\source\repos    
         (this is where your code goes)");
+
+Console.Write(@"c:\invoices");
+
+// Kon'nichiwa World
+Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
