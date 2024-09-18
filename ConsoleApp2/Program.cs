d4b0284f8c16a341311211144ea2ac789687750f
M.Ngugi <mngugi7@gmail.com>
2024-09-18 22:16:53 +0300

    ConsoleA…/ConsoleApp2.dll

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,4 +2,12 @@
 
 Console.WriteLine("Hello\nWorld!");
 Console.WriteLine("Hello\tWorld!");
-Console.WriteLine(" Hello "Hello"!");
+//Console.WriteLine(" Hello "Hello"!");
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
--- ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net8.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f38ad01809af33fa62bc9fc89af422895ac55a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d617e8b17b17612f9b558f21e3c16a316a688bcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
