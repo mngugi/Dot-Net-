5df2738516e7ed6c4ffa9f8c8b62ced6d6c2d074
M.Ngugi <mngugi7@gmail.com>
2024-09-19 23:54:58 +0300

    ConsoleApp3/bin…/ConsoleApp3.dll

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,3 +10,8 @@ Console.WriteLine(message_1);
 
 string projectName = "First-Project";
 Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+//string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+Console.WriteLine(russianMessage);
