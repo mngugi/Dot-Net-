c265ddb63da1cf638f8b96a6603326b202c8379e
M.Ngugi <mngugi7@gmail.com>
2024-09-19 23:44:14 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,3 +2,8 @@
 string greeting = "Hello ";
 string message = greeting + " " + firstName + "!";
 Console.WriteLine(message);
+
+int version = 11;
+string updateText = "Update to Windows";
+string message_1 = $"{updateText} {version}";
+Console.WriteLine(message_1);
